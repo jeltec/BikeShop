4f5bb9abf546362daba1fdda8a60eb62c3776dc1
Jelte Crabbe <jeltecrabbe@gmail.com>
2017-12-12 17:01:38 +0000

    Refactoring before Webpack

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -4,27 +4,27 @@ app.config(function($routeProvider) {
     $routeProvider
     // route for the home page
         .when('/', {
-            templateUrl : 'pages/home.ejs',
+            templateUrl : 'public/pages/home.ejs',
             controller  : 'mainController'
         })
         // route for the donate page
         .when('/donate', {
-            templateUrl : 'pages/donate.ejs',
+            templateUrl : 'public/pages/donate.ejs',
             controller  : 'donateController'
         })
         // route for the donations page
         .when('/donations', {
-            templateUrl : 'pages/donations.ejs',
+            templateUrl : 'public/pages/donations.ejs',
             controller  : 'donationsController'
         })
         // route for the about page
         .when('/about', {
-            templateUrl : 'pages/about.ejs',
+            templateUrl : 'public/pages/about.ejs',
             controller  : 'aboutController'
         })
         // route for the contact page
         .when('/contact', {
-            templateUrl : 'pages/contact.ejs',
+            templateUrl : 'public/pages/contact.ejs',
             controller  : 'contactController'
         });
 });
